6dec5700cb090b20bcbf98616aa07c076192f1e0
xionghaoo <xionghao930316@gmail.com>
2022-12-13 11:04:36 +0800

    25点标定

--- app/src/main/java/com/ubt/robocontroller/BootBroadcastReceiver.kt
+++ app/src/main/java/com/ubt/robocontroller/BootBroadcastReceiver.kt
@@ -12,13 +12,13 @@ import java.io.File
 class BootBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == "android.intent.action.BOOT_COMPLETED") {
-            ToastUtil.show(context, "启动触控服务")
             val downloadDir =
                 Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
             val f1 = File(downloadDir, "module/touchscreen/userdata/Homography.dat")
             val f2 = File(downloadDir, "module/touchscreen/userdata/ThresholdTemplate.dat")
             val pointFile = File(downloadDir, "MarkPoints.json")
             if (pointFile.exists() && f1.exists() && f2.exists()) {
+                ToastUtil.show(context, "启动触控服务")
                 // 标注点文件和触控数据存在时启动触控运行服务
                 val i = Intent(context, UVCService::class.java)
                 i.putExtra(UVCService.EXTRA_BOOT_CMD, true)
